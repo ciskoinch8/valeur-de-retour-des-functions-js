0b9d7737364931cc559560b6ca0176c7501f70bb
ciskoinch8 <cissokhomoussa08@outlook.fr>
2022-03-26 23:25:45 +0100

    la valeur retournée des fonctions

---
+++ main.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('.numberInput');
+const para = document.querySelector('h3');
+
+function squared(num){
+    return num * num;    
+}
+
+function cubed(num){
+    return num * num * num;
+}
+
+function factorial(num){
+    let x = num;
+    while(x > 1){
+        num *= x-1;
+        x--;
+    }
+    return num;
+}
+
+input.onchange = function() {
+    let num = input.value;
+    if (isNaN(num)) {
+      para.textContent = 'You need to enter a number!';
+    } else {
+      para.textContent = `${num} squared is ${squared(num)}.\n\n ${num} cubed is ${cubed(num)}. ${num} factorial is ${factorial(num)}`;
+    }
+  }
+  
